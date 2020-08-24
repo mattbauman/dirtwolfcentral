f28e5a91b219d94ef90bb707ffca37a3d74d9189
mattbauman <matt@mattbauman.com>
2020-08-24 09:19:27 -0500

    adding express js

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const https = require('https');
+const fs = require('fs');
+
+//GET home route
+app.get('/', (req, res) => {
+    res.send('Welcome to Dirtwolf Central');
+});
+
+var routes = require('./routes.js');
+
+//both index.js and routes.js should be in same directory
+app.use('/routes', routes);
+
+// we will pass our 'app' to 'https' server
+https.createServer({
+    key: fs.readFileSync('/opt/bitnami/letsencrypt/certificates/dirtwolfcentral.com.key'),
+    cert: fs.readFileSync('/opt/bitnami/letsencrypt/certificates/dirtwolfcentral.com.crt')
+//    passphrase: 'YOUR PASSPHRASE HERE'
+}, app)
+.listen(3000);
